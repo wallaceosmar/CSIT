3d95b23ff6e069183d4cf226d3ff141c39759e22
jozielpn <joziel-pn@hotmail.com>
2015-09-18 15:17:08 -0300

    Update simplex.php

--- simplex.php
+++ simplex.php
@@ -1,2 +1,14 @@
 <?php
+   //Variáveis
+   $v = "Teste Variavel";
+   
+   //Constantes
+   define("c","Teste Constante");
+   define("phpversion","PHP_VERSION");
+   define("dirfile","__FILE__");
+   define("os","PHP_OS");
+   
+   echo "Variavel --> $v"."<br/>";
+   echo "Constante --> ".c."<br/>";
+   
 ?>
